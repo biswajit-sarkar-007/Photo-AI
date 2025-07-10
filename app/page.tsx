28c17c53df85a58fef18a09462f7742d1717881d
Biswajit Sarkar <biswajitsarkar0485@gmail.com>
2025-07-10 13:18:46 +0530

    updated some pages

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,29 @@
-"use client"
+"use client";
 import Editor from "@/components/Editor";
 import { ImageStore } from "@/lib/image-store";
+import { LayerStore } from "@/lib/layer-store";
 
- export default function Home (){
+export default function Home() {
   return (
-    <ImageStore.Provider initialValue={{generating:false}}>
-      <main>
-      <Editor/>
-
-    </main>
-
-    </ImageStore.Provider>
-    
-    
-  )
- }
+    <LayerStore.Provider
+      initialValue={{
+        layerComparisonMode: false,
+        layers: [
+          {
+            id: crypto.randomUUID(),
+            url: "",
+            height: 0,
+            width: 0,
+            publicId: "",
+          },
+        ],
+      }}
+    >
+      <ImageStore.Provider initialValue={{ generating: false }}>
+        <main className="h-full">
+          <Editor />
+        </main>
+      </ImageStore.Provider>
+    </LayerStore.Provider>
+  );
+}
--- components/Editor.tsx
+++ components/Editor.tsx
@@ -1,12 +1,15 @@
 "use client"
 
+import Layers from "./layers/layers"
 import UploadImage from "./upload/UploadImage"
 
 export default function Edior(){
     return(
-        <div>Edior
+        <div className="flex h-full">
+            
 
             <UploadImage/>
+            <Layers/>
         </div>
     )
 }
