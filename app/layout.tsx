04c4a2cffd0062ce10f774f83eb69fe1db9eda18
Biswajit Sarkar <biswajitsarkar0485@gmail.com>
2025-07-10 13:35:55 +0530

    update2

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { ThemeProvider } from "@/components/theme/theme-provider";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -27,7 +28,11 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <ThemeProvider attribute="class" defaultTheme="system" enableSystem >
+           {children}
+
+        </ThemeProvider>
+       
       </body>
     </html>
   );
--- components/Editor.tsx
+++ components/Editor.tsx
@@ -1,11 +1,19 @@
 "use client"
 
+import { ModeToggle } from "@/components/theme/mode-toggle"
 import Layers from "./layers/layers"
 import UploadImage from "./upload/UploadImage"
 
 export default function Edior(){
     return(
         <div className="flex h-full">
+            <div className="py-6 px-4 basis-[240px] shrink-0">
+                 <div className="pb-12 text-center">
+                <ModeToggle/>
+            </div>
+
+            </div>
+           
             
 
             <UploadImage/>
